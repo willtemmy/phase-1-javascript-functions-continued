76a50da53bca1462e54a63daa99d7925a8105c51
William Mtemi Mponzi <willbrian009@gmail.com>
2023-03-19 18:00:17 +0300

    Trouble with last test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//this is a function expression has no function declaration
+const mondayWork = function (activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(para1="*") {
+    return function (emphatic="special") {
+        return `You are ${para1}.`
+    }
+}
